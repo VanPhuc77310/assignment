f3b6350f85f72776efecaf97194070ff6e0948b6
Văn Phúc <vvphuc28062003@gmail.com>
2025-09-25 18:26:05 +0700

    implement multiply

---
+++ multiply.js
@@ -0,0 +1,10 @@
+var calculate( sign,  arg1,  arg2) => {
+    switch (sign) {
+    case MULTIPLY:
+        return arg1 * arg2;
+    default: 
+        break;
+            
+    }
+    return null;
+}
