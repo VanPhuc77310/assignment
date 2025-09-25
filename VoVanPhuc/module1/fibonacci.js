de677e19bc4eb209a44f6f396ebf867fcd90ea9e
Văn Phúc <vvphuc28062003@gmail.com>
2025-09-25 18:42:53 +0700

    add fibonacci khi flag = false

--- VoVanPhuc/module1/fibonacci.js
+++ VoVanPhuc/module1/fibonacci.js
@@ -1,1 +1,14 @@
-function Fibonacci(n, flag) {}
+function Fibonacci(n, flag) {
+  if (flag === false) {
+    if (n <= 1) return n;
+    let a = 0,
+      b = 1;
+    for (let i = 2; i <= n; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
+  }
+  return null;
+}
